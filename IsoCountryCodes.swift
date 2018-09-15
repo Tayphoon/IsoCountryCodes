a5252cba4aedc6a3f83c8a7711481decf42677d2
Tayphoon <tayphoon.company@gmail.com>
2018-09-15 20:42:07 +0300

    Make class functions public

--- IsoCountryCodes.swift
+++ IsoCountryCodes.swift
@@ -6,31 +6,31 @@
 //  Copyright (c) 2015 Funky Monkey, www.funky-monkey.nl. All rights reserved.
 //
 
-class IsoCountryCodes {
+public class IsoCountryCodes {
 
-    class func find( key:String ) -> IsoCountryInfo {
+    public class func find( key:String ) -> IsoCountryInfo {
         var country = IsoCountries.allCountries.filter(  { $0.alpha2 == key.uppercased() || $0.alpha3 == key.uppercased() || $0.numeric == key } )
         return country[0]
     }
 
-    class func searchByName( name:String ) -> IsoCountryInfo {
+    public class func searchByName( name:String ) -> IsoCountryInfo {
         var country = IsoCountries.allCountries.filter( { $0.name == name } )
 
         return (!country.isEmpty) ? country[0] : IsoCountryInfo(name: "", numeric: "", alpha2: "", alpha3: "", calling: "", currency: "", continent: "")
     }
 
-    class func searchByNumeric( numeric:String ) -> IsoCountryInfo {
+    public class func searchByNumeric( numeric:String ) -> IsoCountryInfo {
         let country = IsoCountries.allCountries.filter( { $0.numeric == numeric } )
 
         return (!country.isEmpty) ? country[0] : IsoCountryInfo(name: "", numeric: "", alpha2: "", alpha3: "", calling: "", currency: "", continent: "")
     }
 
-    class func searchByCurrency( currency:String ) -> [IsoCountryInfo] {
+    public class func searchByCurrency( currency:String ) -> [IsoCountryInfo] {
         let country = IsoCountries.allCountries.filter(  { $0.currency == currency } )
         return country
     }
 
-    class func searchByCallingCode( calllingCode:String ) -> IsoCountryInfo {
+    public class func searchByCallingCode( calllingCode:String ) -> IsoCountryInfo {
         var country = IsoCountries.allCountries.filter( { $0.calling == calllingCode } )
         return country[0]
     }
